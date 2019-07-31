ff559dc925dcbded20e8442e799fd581d2c4a38b
jamiefosternz <jamiefosternz7@gmail.com>
2019-07-31 12:23:33 +1200

    Added display functionality
    Added functions to display certain information based on system state.  Currently only pre deliv/pickup working.  Changed some variables to keep continuity.

--- main.py
+++ main.py
@@ -1,15 +1,91 @@
 #import dependencies
 import os
+import time
 
-#init variables
+#init global cust. variables
 cust_name = ""
-address = ""
-ph_number = 0
-pizza_number = 0
+cust_address = ""
+cust_ph_number = None 
+cust_pizza_number = None
 
 order_cost = 0
 
+#constant variables
 pizzas = ["Cheese", "Pepperoni", "Hawaiian", "Veggie", "Beef & Onion", "Garlic Cheese", "Ham & Cheese", "Butter Chicken", "Shrimp", "Meatlovers", "Apricot Chicken", "Supreme"]
-pizza_prices = [10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 15.5, 15.5, 15.5, 15.5, 15.5, ]
+pizza_prices = [10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 15.5, 15.5, 15.5, 15.5, 15.5]
 
 
+#display related variables
+seperator = "*********************************************************"
+
+
+
+#main function to printout.  Integer input for state of program to print out specific page -
+	#pre deliv/pickup   = 1
+	#cust. info input	= 2
+	#confirm order		= 3
+	#display order 		= 4
+
+def printout(x):
+	os.system("cls")
+	if x == 1:
+		title()
+		blankline(2)
+		custInfo()
+		blankline(1)
+		print(seperator)
+	elif x == 2:
+		title()
+	elif x == 3:
+		title()
+	elif x == 4:
+		title()
+	else:
+		print("Output error in printout(). Halting...")
+		exit()
+			
+#functions used in printout() to simplify process
+def title():
+	print(seperator)
+	print("************** Pizza Order Management v1.0 **************")
+	print(seperator) 
+
+def blankline(x):
+	for i in range(x):
+		print("")
+
+def custInfo():
+	print("Customer name: ", end="")
+	if cust_name != "":
+		print(cust_name)
+	else:
+		print("")
+	
+	print("Customer address: ", end="")
+	if cust_address != "":
+		print(cust_address)
+	else:
+		print("")
+	
+	print("Customer phone number: ", end="")
+	if cust_ph_number != None:
+		print(cust_ph_number)
+	else:
+		print("")
+	
+	print("Total number of pizzas ordered: ", end="")
+	if cust_pizza_number != None:
+		print(cust_pizza_number)
+	else:
+		print("")
+		
+	print("Total order cost: $", end="")
+	print(order_cost)
+
+
+
+
+
+while True:
+	printout(1)
+	time.sleep(1)
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
+mode con: cols=57 lines=30
 "C:\Program Files\Python\python.exe" "H:\program root\main.py"
 pause
