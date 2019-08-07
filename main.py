e3de59974bdf6ab1823a8785d4f8dff18946927a
jamiefosternz <jamiefosternz7@gmail.com>
2019-08-08 10:01:48 +1200

    Add functionality for displaying pizzas, added user input
    Added user input up to inputting specific pizzas, edited printOut() to display available pizzas

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ cust_name = ""
 cust_address = ""
 cust_ph_number = None 
 cust_pizza_number = None
-
+cust_pizzas = []
 order_cost = 0
+cust_delivery = None
 
 #constant variables
 pizzas = ["Cheese", "Pepperoni", "Hawaiian", "Veggie", "Beef & Onion", "Garlic Cheese", "Ham & Cheese", "Butter Chicken", "Shrimp", "Meatlovers", "Apricot Chicken", "Supreme"]
@@ -16,7 +17,9 @@ pizza_prices = [10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 10.5, 15.5, 15.5, 15.5, 15.5
 
 
 #display related variables
-seperator = "*********************************************************"
+seperator = "***********************************************************************"
+flatSeperator = "-----------------------------------------------------------------------"
+pizzaLines = 5
 
 
 
@@ -25,17 +28,30 @@ seperator = "*********************************************************"
 	#cust. info input	= 2
 	#confirm order		= 3
 	#display order 		= 4
-
-def printout(x):
+	
+def printOut(x):
 	os.system("cls")
 	if x == 1:
 		title()
-		blankline(2)
+		blankLine(1)
 		custInfo()
-		blankline(1)
+		blankLine(1)
 		print(seperator)
+		currentPizzas()
+		print(seperator)
+		
 	elif x == 2:
 		title()
+		blankLine(1)
+		custInfo()
+		blankLine(1)
+		print(seperator)
+		currentPizzas()
+		print(seperator)
+		print("Available pizzas:")
+		for i in range(len(pizzas)):
+			print("{}: {}".format(i+1, pizzas[i]))
+		print(seperator)
 	elif x == 3:
 		title()
 	elif x == 4:
@@ -47,45 +63,100 @@ def printout(x):
 #functions used in printout() to simplify process
 def title():
 	print(seperator)
-	print("************** Pizza Order Management v1.0 **************")
+	print("********************* Pizza Order Management v1.0 *********************")
 	print(seperator) 
 
-def blankline(x):
+def blankLine(x):
 	for i in range(x):
 		print("")
 
 def custInfo():
-	print("Customer name: ", end="")
+	print(flatSeperator)
+	print("Customer name                  | ", end="")
 	if cust_name != "":
 		print(cust_name)
 	else:
 		print("")
+		
 	
-	print("Customer address: ", end="")
-	if cust_address != "":
-		print(cust_address)
-	else:
-		print("")
 	
-	print("Customer phone number: ", end="")
-	if cust_ph_number != None:
-		print(cust_ph_number)
-	else:
-		print("")
-	
-	print("Total number of pizzas ordered: ", end="")
+	print(flatSeperator)
+	print("Total number of pizzas ordered | ", end="")
 	if cust_pizza_number != None:
 		print(cust_pizza_number)
 	else:
 		print("")
 		
-	print("Total order cost: $", end="")
+	print(flatSeperator)	
+	print("Total order cost               | $", end="")
 	print(order_cost)
+	
+	print(flatSeperator)
+	if cust_delivery == True:
+		print("Customer address               | ", end="")
+		if cust_address != "":
+			print(cust_address)
+		else:
+			print("")
+		print(flatSeperator)
+		
+		print("Customer phone number          | ", end="")
+		if cust_ph_number != None:
+			print(cust_ph_number)
+		else:
+			print("")
+		print(flatSeperator)
+	else:
+		blankLine(4)
 
+def currentPizzas():
+	print("Current Pizzas:")
+	for i in range(len(cust_pizzas)):
+		print(cust_pizzas[i])
+		pizzalines += -1
+	blankLine(pizzaLines)
+	
+#input functions
+def deliveryQuery():
+	while True:
+		cust_input = input("Is this order for delivery? (y/n) >")
+		if cust_input.lower() == "y" or cust_input.lower() == "n":
+			if cust_input.lower() == "y":
+				return True
+			else:
+				return False
+		printOut(1)
+		print("Wrong input, try again")
 
-
-
+def orderSizeQuery():
+	while True:
+		try:
+			cust_input = int(input("How many pizzas is the customer ordering? (max 5) >"))
+			if cust_input > 0 and cust_input < 6:
+				return cust_input
+			printOut(1)
+			print("Wrong input, try again")
+		except:
+			printOut(1)
+			print("That's not a valid number.  Try again.")
 
 while True:
-	printout(1)
+	printOut(1)
+	cust_delivery = deliveryQuery()
+	
+	printOut(1)
+	cust_name = input("What is the customer's name? >")
+	
+	printOut(1)
+	if cust_delivery == True:
+		cust_address = input("What is the customer's address? >")
+		
+		printOut(1)
+		cust_ph_number = input("What is the customer's phone number? >")
+
+		printOut(1)
+	
+	cust_pizza_number = orderSizeQuery()
+	
+	printOut(2)
 	time.sleep(1)
--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
-mode con: cols=57 lines=30
+mode con: cols=71 lines=50
+COLOR 70
+title Pizza order
 "C:\Program Files\Python\python.exe" "H:\program root\main.py"
 pause
